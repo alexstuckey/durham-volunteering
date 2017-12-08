df7396cd649f4c9625b1a3c491eafd495bed20b9
Alex Stuckey <alex@stuckey.org.uk>
2017-12-08 13:48:42 +0000

    Importing of sourcemaps and sass for gulp

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+var sourcemaps = require('gulp-sourcemaps')
+var sass = require('gulp-sass')
 
 gulp.task('default', function() {
   // place code for your default task here
-});
+})
