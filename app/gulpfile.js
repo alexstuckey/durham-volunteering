9e56ab0841613f208a71c1843efbb7083f1ac342
Alex Stuckey <alex@stuckey.org.uk>
2017-12-10 18:22:39 +0000

    Gulp: js routine, just copies

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -9,3 +9,11 @@ gulp.task('sass', function () {
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('./static/css'));
 });
+
+// Move JS Files to ./static/js
+gulp.task('js', function() {
+  return gulp.src(['./src/js/**/*.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./static/js"))
+});
