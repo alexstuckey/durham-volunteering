7b3ae36918d77f74d860aa13a6907d43fc026dfd
williamejelias <32936120+williamejelias@users.noreply.github.com>
2017-12-15 23:51:39 +0000

    Update homepage.js

--- app/application/javascript/homepage.js
+++ app/application/javascript/homepage.js
@@ -2,5 +2,8 @@ function foo() {
     return 'bar';
 }
 
+// access Notification Well elements using .notification (using this applies to all notification elements)
+
+// access dismiss/archive buttons using .dismiss/.archive (applies to all dismiss/archive buttons
 console.log(foo());
 console.log('javascript is functional');
