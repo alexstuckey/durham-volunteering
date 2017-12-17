ef3d8ffb65a482855bb007c95c8b5f4969892eba
Alex Stuckey <alex@stuckey.org.uk>
2017-12-17 23:01:11 +0000

    Routes for causes

--- app/application/config/routes.php
+++ app/application/config/routes.php
@@ -52,3 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['causes']['GET'] = 'causes/list'
+$route['causes']['POST'] = 'causes/create'
+
+$route['cause/(:num)']['GET'] = 'causes/get/'
