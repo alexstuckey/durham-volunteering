936f00c47a61c7ded078b1d0f7e05df6d385e3ba
williamejelias <32936120+williamejelias@users.noreply.github.com>
2017-12-18 20:49:41 +0000

    Update homepage.js

--- app/src/js/homepage.js
+++ app/src/js/homepage.js
@@ -2,6 +2,15 @@ function foo() {
     return 'bar';
 }
 
+let offset = 80;
+
+$('body').scrollspy
+$('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
+
 // access Notification Well elements using .notification (using this applies to all notification elements)
 
 // access dismiss/archive buttons using .dismiss/.archive (applies to all dismiss/archive buttons
