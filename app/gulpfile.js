b4016900058c14a16d614cd2a82909c5633fd913
Alex Stuckey <alex@stuckey.org.uk>
2018-01-18 15:24:01 +0000

    Needed to use UMD module packaging

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass', function () {
 
 // Move JS Files to ./static/js
 gulp.task('js', function() {
-  return gulp.src(['./src/js/**/*.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js', 'node_modules/popper.js/dist/popper.min.js'])
+  return gulp.src(['./src/js/**/*.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest("./static/js"))
