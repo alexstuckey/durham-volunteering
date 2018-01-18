b2f9c0a8b8e664323e63331b1f27d9d8587dd8a3
Alex Stuckey <alex@stuckey.org.uk>
2018-01-18 10:55:27 +0000

    Route for onboarding

--- app/application/config/routes.php
+++ app/application/config/routes.php
@@ -53,6 +53,8 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+$route['onboard/welcome'] = 'onboarding/welcome';
+
 $route['causes']['GET'] = 'causes/listAll';
 $route['causes']['POST'] = 'causes/create';
 
