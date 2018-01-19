9d6bfac6dab47061f3bc8299c62f3dffcb51a318
AninaAntonie <anina.koehler@live.de>
2018-01-19 01:11:13 +0000

    added text to the forms of the onboarding process

--- app/application/views/onboarding_3_enter_details_form.php
+++ app/application/views/onboarding_3_enter_details_form.php
@@ -44,6 +44,9 @@
                         <div id="volunteering">
                             <h1>Enter Your Details</h1>
 
+                            <h4><br /></h4>
+                            <p>Please provide further information about yourself that will be added to your profile. We will use the name provided for contacting you and interacting with your manager.<br /></p>
+
                             <div class="card">
 
                                 <div class="card-block">
--- app/application/views/onboarding_5_nominate_manager_form.php
+++ app/application/views/onboarding_5_nominate_manager_form.php
@@ -44,6 +44,10 @@
                         <div id="volunteering">
                             <h1>Nominate Your Manager</h1>
 
+                            <h4><br /></h4>
+                            <p>As a member of staff you have the opportunity to volunteer during your working hours. However this is subject to a line-manager approval.<br /><br />Please use the form provided below to nominate your manager. We will send them an email containing links for accepting or rejecting the nomination. If the nomination gets rejected you will be able to nominate someone else.<br /></p>
+
+
                             <div class="card">
                                 <div class="card-block">
                                     <form action="send_nominate_manager" method="post">
