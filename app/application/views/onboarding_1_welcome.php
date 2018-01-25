a4f7cfbb59e9bf1bd1a49c86a01d9ea81b684780
AninaAntonie <anina.koehler@live.de>
2018-01-25 10:59:01 +0000

    urls

--- app/application/views/onboarding_1_welcome.php
+++ app/application/views/onboarding_1_welcome.php
@@ -60,22 +60,19 @@
             </div>
 
             <!-- FOOTER -->
-            <h1><br /></h1>
 
             <div id="footer">
                 <footer class="container text-center">
-                    <h4><br /></h4>
-                    <h2>We'd love to hear from you</h2>
-                    <h4><br /></h4>
+                    <h2 id="footer-heading">We'd love to hear from you</h2>
                     <p><b>Staff Volunteering & Outreach Team</b></p>
                     <a href="mailto:community.engagement@durham.ac.uk">
-                        <img class="contact_icon" src="../../src/images/mail.png" width="25px">
+                        <img class="contact_icon" src="<?php echo base_url('/src/images/mail.png'); ?>" width="25px">
                     </a>
                     <a href="https://www.facebook.com/SVODurham/">
-                        <img class="contact_icon" src="../../src/images/FB-f-Logo.png" width="25px">
+                        <img class="contact_icon" src="<?php echo base_url('/src/images/FB-f-Logo.png.png'); ?>" width="25px">
                     </a>
                     <a href="tel:0191 334 2199">
-                        <img class="contact_icon" src="../../src/images/telephone.png" width="25px">
+                        <img class="contact_icon" src="<?php echo base_url('/src/images/telephone.png'); ?>" width="25px">
                     </a>
                 </footer>
 
