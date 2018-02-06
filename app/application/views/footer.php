839032ae852fd3253b0d6e38702bc773f86dcc99
Alex Stuckey <alex@stuckey.org.uk>
2018-02-06 13:23:43 +0000

    Seperation of home into three files

---
+++ app/application/views/footer.php
@@ -0,0 +1,27 @@
+            <!-- FOOTER -->
+            <div id="footer">
+                <footer class="container text-center">
+                    <h4><br /></h4>
+                    <h2>We'd love to hear from you</h2>
+                    <h4><br /></h4>
+                    <p><b>Staff Volunteering & Outreach Team</b></p>
+                    <a href="mailto:community.engagement@durham.ac.uk">
+                        <img class="contact_icon" src="<?php echo base_url('/static/images/mail.png'); ?>" width="25px">
+                    </a>
+                    <a href="https://www.facebook.com/SVODurham/">
+                        <img class="contact_icon" src="<?php echo base_url('/static/images/FB-f-Logo.png'); ?>" width="25px">
+                    </a>
+                    <a href="tel:0191 334 2199">
+                        <img class="contact_icon" src="<?php echo base_url('/static/images/telephone.png'); ?>" width="25px">
+                    </a>
+                </footer>
+
+                <footer class="container text-center">
+                    <hr>
+                    <p><br /><b>design & programming</b><br />software engineering team</p>
+                </footer>
+
+            </div>
+        </div>
+    </body>
+</html>
---
+++ app/application/views/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <?php $this->load->helper('url'); ?>
+        
+        <!-- Popper JS -->
+        <script src="<?php echo base_url('/static/js/popper.min.js'); ?>"></script>
+        
+        <!-- Vendor JS -->
+        <script src="<?php echo base_url('/static/js/jquery.min.js'); ?>"></script>
+        <script src="<?php echo base_url('/static/js/bootstrap.min.js'); ?>"></script>
+        <script src="<?php echo base_url('/static/js/Chart.min.js'); ?>"></script>
+        
+        <!-- App JS -->
+        <script src="<?php echo base_url('/static/js/homepage.js'); ?>"></script>
+
+        <!-- Vendor CSS -->
+        <link rel="stylesheet" href="<?php echo base_url('/static/css/bootstrap.css'); ?>">
+
+        <!-- App CSS -->
+        <link rel="stylesheet" href="<?php echo base_url('/static/css/homepage.css'); ?>" type="text/css">
+
+    </head>
+
+    <body>
+        <!-- Webpage -->
+        <div id="page">
+            <div id="nav">
+                <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+                    <a class="navbar-brand" href="#">Staff Volunteering Programme</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#notifications">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#volunteering">Volunteering</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#managerShiftResponse">Respond to Applications</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#otherSection">Other Section</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            
