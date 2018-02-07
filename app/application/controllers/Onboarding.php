8b91c35b67f866da41ac14180e51a48e857444eb
Alex Stuckey <alex@stuckey.org.uk>
2018-02-07 18:36:36 +0000

    Updated the rest of the pages

--- app/application/controllers/Onboarding.php
+++ app/application/controllers/Onboarding.php
@@ -38,27 +38,67 @@ class Onboarding extends CI_Controller {
 
 	public function enter_details_form()
 	{
-		$data['cis_username'] = 'xxxx99';
+		if (!$this->user_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+			// First access by user, normally redirect, but already there.
+			$this->load->helper('url');
+			redirect('/onboard/welcome');
+		}
+
+		$user_fetch = $this->user_model->getUserByCIS($_SERVER['REMOTE_USER']);
+		$data['user'] = $user_fetch[0];
+
 		$this->load->view('onboarding_3_enter_details_form', $data);
+
+		$this->user_model->setOnboardingStatus($_SERVER['REMOTE_USER'], 3);
 	}
 
 	public function send_details()
 	{
-		$data['cis_username'] = 'xxxx99';
+		if (!$this->user_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+			// First access by user, normally redirect, but already there.
+			$this->load->helper('url');
+			redirect('/onboard/welcome');
+		}
+
+		$user_fetch = $this->user_model->getUserByCIS($_SERVER['REMOTE_USER']);
+		$data['user'] = $user_fetch[0];
+		
 		$data['active'] = 'nominate_manager';
 		$this->load->view('onboarding_steps', $data);
+
+		$this->user_model->setOnboardingStatus($_SERVER['REMOTE_USER'], 4);
 	}
 
 	public function enter_nominate_manager_form()
 	{
-		$data['cis_username'] = 'xxxx99';
+		if (!$this->user_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+			// First access by user, normally redirect, but already there.
+			$this->load->helper('url');
+			redirect('/onboard/welcome');
+		}
+
+		$user_fetch = $this->user_model->getUserByCIS($_SERVER['REMOTE_USER']);
+		$data['user'] = $user_fetch[0];
+
 		$this->load->view('onboarding_5_nominate_manager_form', $data);
+
+		$this->user_model->setOnboardingStatus($_SERVER['REMOTE_USER'], 5);
 	}
 
 	public function send_nominate_manager()
 	{
-		$data['cis_username'] = 'xxxx99';
+		if (!$this->user_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+			// First access by user, normally redirect, but already there.
+			$this->load->helper('url');
+			redirect('/onboard/welcome');
+		}
+
+		$user_fetch = $this->user_model->getUserByCIS($_SERVER['REMOTE_USER']);
+		$data['user'] = $user_fetch[0];
+
 		$data['active'] = 'get_started';
 		$this->load->view('onboarding_steps', $data);
+
+		$this->user_model->setOnboardingStatus($_SERVER['REMOTE_USER'], 6);
 	}
 }
