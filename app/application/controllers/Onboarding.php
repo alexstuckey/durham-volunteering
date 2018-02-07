a9cd3a5dfc1e7f1a2f09333429ccb6aaa821373e
Alex Stuckey <alex@stuckey.org.uk>
2018-02-07 18:32:40 +0000

    Onboarding for 2_step_details

--- app/application/controllers/Onboarding.php
+++ app/application/controllers/Onboarding.php
@@ -20,9 +20,20 @@ class Onboarding extends CI_Controller {
 
 	public function step_details()
 	{
-		$data['cis_username'] = 'xxxx99';
+		if (!$this->user_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+			// First access by user, normally redirect, but already there.
+			$this->load->helper('url');
+			redirect('/onboard/welcome');
+		}
+
+		$user_fetch = $this->user_model->getUserByCIS($_SERVER['REMOTE_USER']);
+		$data['user'] = $user_fetch[0];
+
 		$data['active'] = 'enter_details';
+		
 		$this->load->view('onboarding_steps', $data);
+
+		$this->user_model->setOnboardingStatus($_SERVER['REMOTE_USER'], 2);
 	}
 
 	public function enter_details_form()
