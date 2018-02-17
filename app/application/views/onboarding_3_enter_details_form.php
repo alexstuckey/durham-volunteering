81d92803c8209b277c28008b5524e2c4aeacdc9a
Alex Stuckey <alex@stuckey.org.uk>
2018-02-17 23:13:34 +0000

    Giving names to the inputs (+autocomplete)

--- app/application/views/onboarding_3_enter_details_form.php
+++ app/application/views/onboarding_3_enter_details_form.php
@@ -55,12 +55,12 @@
 
                                         <div class="form-group">
                                             <label for="exampleInputEmail1">First Name</label>
-                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter first name">
+                                            <input type="text" autocomplete="given-name" class="form-control" name="inputFirstName" id="inputFirstName" placeholder="Enter first name">
                                         </div>
 
                                         <div class="form-group">
                                             <label for="exampleInputEmail1">Last Name</label>
-                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter last name">
+                                            <input type="text" autocomplete="family-name" class="form-control" name="inputLastName" id="inputLastName" placeholder="Enter last name">
                                         </div>
 
                                         <button type="submit" class="btn btn-primary">Submit</button>
