21d56051c6f77a7e62d3d4f3f478d262959232df
williamejelias <wj.elias@virgin.net>
2018-02-19 20:52:44 +0000

    closed php tags

--- app/application/views/header.php
+++ app/application/views/header.php
@@ -38,16 +38,16 @@
                     </button>
                     <div class="collapse navbar-collapse" id="collapsibleNavbar">
                         <ul class="navbar-nav">
-                            <li class="nav-item<?php if ($active !== "home") { echo " active"; }">
+                            <li class="nav-item<?php if ($active !== "home") { echo " active"; }; ?>">
                                 <a class="nav-link" href="">Home</a>
                             </li>
-                            <li class="nav-item<?php if ($active !== "volunteering") { echo " active"; }">
+                            <li class="nav-item<?php if ($active !== "volunteering") { echo " active"; }; ?>">
                                 <a class="nav-link" href="">Volunteering</a>
                             </li>
-                            <li class="nav-item<?php if ($active !== "applciations") { echo " active"; }">
+                            <li class="nav-item<?php if ($active !== "applciations") { echo " active"; }; ?>">
                                 <a class="nav-link" href="">Respond to Applications</a>
                             </li>
-                            <li class="nav-item<?php if ($active !== "other") { echo " active"; }">
+                            <li class="nav-item<?php if ($active !== "other") { echo " active"; }; ?>">
                                 <a class="nav-link" href="">Other Section</a>
                             </li>
                         </ul>
