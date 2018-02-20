acda74e7f74f1c85c8f02de67890a6241ce31fa8
williamejelias <wj.elias@virgin.net>
2018-02-20 14:16:52 +0000

    Added new routes - defined controller functions to deliver html chunks as separate pages as a way of removing the scrollspy

--- app/application/views/header.php
+++ app/application/views/header.php
@@ -39,16 +39,16 @@
                     <div class="collapse navbar-collapse" id="collapsibleNavbar">
                         <ul class="navbar-nav">
                             <li class="nav-item<?php if ($active == "home") { echo " active"; }; ?>">
-                                <a class="nav-link" href="">Home</a>
+                                <a class="nav-link" href="<?php echo base_url('/home'); ?>">Home</a>
                             </li>
                             <li class="nav-item<?php if ($active == "volunteering") { echo " active"; }; ?>">
-                                <a class="nav-link" href="">Volunteering</a>
+                                <a class="nav-link" href="<?php echo base_url('/volunteering'); ?>">Volunteering</a>
                             </li>
                             <li class="nav-item<?php if ($active == "applciations") { echo " active"; }; ?>">
-                                <a class="nav-link" href="">Respond to Applications</a>
+                                <a class="nav-link" href="<?php echo base_url('/manager'); ?>">Respond to Applications</a>
                             </li>
                             <li class="nav-item<?php if ($active == "other") { echo " active"; }; ?>">
-                                <a class="nav-link" href="">Other Section</a>
+                                <a class="nav-link" href="<?php echo base_url('/other'); ?>">Other Section</a>
                             </li>
                         </ul>
                     </div>
