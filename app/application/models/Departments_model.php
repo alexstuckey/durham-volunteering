56ae0f3c2ea7d5df8ec08afa39f0b77fa0d8c116
original <jonathan.a.digweed@durham.ac.uk>
2018-03-21 14:53:22 +0000

    Departments model completed

--- app/application/models/Departments_model.php
+++ app/application/models/Departments_model.php
@@ -13,26 +13,52 @@ class Departments_model extends CI_Model {
     // Returns an array of deparments, with their id and name
     public function getDepartmentsList()
     {
+        $this->db->select('name');
+        $this->db->from('departments');
+
+        $query = $this->db->get();
+
+        $data=$query->result_array();
+
+        return $data;
 
     }
 
     // Returns an array of deparments, with their id, name, and count of members
     public function getDepartmentsListWithCount()
     {
+        $this->db->select('departments, COUNT(departments) as total');
 
+        $this->db->join('departments','departments.ID=users.departmentID');
+
+
+        $this->db->group_by('departments');
+        $this->db->order_by('total', 'desc');
+
+
+        $query = $this->db->get();
+
+        $data=$query->result_array();
+
+        return $data
     }
 
 
     // Makes a new department, with the given name
-    public function newDepartment($newName)
+    public function newDepartment($data)
     {
+
+
+            $this->db->insert('departments', $data);
+
 
     }
 
     // Edits a department, by an id to change it's name
-    public function editDepartment($id, $newName)
+    public function editDepartment($id, $data)
     {
-
+        $this->db->where('id', $id);
+        $this->db->update('departments', $data);
     }
 
 
