03226b900a8747e891d223293aa521f5e48c2ee0
Alex Stuckey <alex@stuckey.org.uk>
2018-03-22 00:12:28 +0000

    Making audit model

---
+++ app/application/models/Audit_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Audit_model extends CI_Model {
+
+    //Loads the database using the ../config/database.php file
+    public function __construct()	{
+        $this->load->database();
+    }
+
+    public function getWholeAuditTrail()
+    {
+        $query = $this->db->get('audit');
+
+        return $query->result_array();
+    }
+
+}
