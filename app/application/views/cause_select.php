8a8fefaef762ee6e869c45234e762a4a3c9bf00d
williamejelias <wj.elias@virgin.net>
2018-03-24 20:11:06 +0000

    add select2 dropdown and make each value link to cause 1

--- app/application/views/cause_select.php
+++ app/application/views/cause_select.php
@@ -1,6 +1,23 @@
 <!-- Cause select div -->
+<div class="card">
+    <div class="card-header">
+        <h4>View Causes</h4>
+    </div>
+    <div class="card-block">
 
-<p> SELECT 2 DROPDOWN WITHIN CARD </p>
+        <!-- Select 2 dropdown to select cause. value should hyperlink to the single cause functionality to load in cause html stub -->
+        <form>
+            <div class="form-group">
+                <label for="shiftApplicationCause">Cause</label>
+                <select class="form-control" name="shiftApplicationCause" id="shiftApplicationCause">
+                    <?php foreach ($causes as $cause): ?>
+                        <option value="<?php echo $cause['id']; ?>"><a href="<?php echo base_url('index.php/cause/1'); ?>"><?php echo $cause['organisation'] ?></a></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+        </form>
+    </div>
+</div>
 
 <p> data of the single cause selected in above dropdown</p>
 
