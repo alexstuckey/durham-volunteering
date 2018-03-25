1e284d3bdcb3660b1d2face9b6c226423e81d56a
Alex Stuckey <alex@stuckey.org.uk>
2018-03-25 16:28:43 +0100

    Improving createTime model method

--- app/application/models/Time_model.php
+++ app/application/models/Time_model.php
@@ -18,44 +18,33 @@ class Time_model extends CI_Model {
         return $query->result_array();
     }
 
-//  Takes an array of the form
-//  $data = array(
-// 'cisID' => 'cisID',
-// 'start' => 'start Time',
-// 'finish' => 'finish Time',
-// 'causeID' => 'causeID',
-//groupEvent' => 'groupEvent'
-// );
 // Inserts into 'time' table in the database.
-    public function createTime($data)
+    public function createTime($CISID, $start, $end, $causeID, $comment)
     {
-
-        $valid_flag=True;
-
-        $causeExistsFlag=False;
-
-
-        $this->db->where('causeID', $data['causeID']);
-
+        $this->db->where('causeID', $causeID);
         $query = $this->db->get('causes');
+        $cause = $query->row_array();
 
-        $result=$query->result_array();
+        $causeExists = False;
 
-
-        if(sizeof($result)>=1)
+        if(!empty($cause))
         {
-            $causeExistsFlag=True;
+            $causeExists = True;
         }
 
-        if($valid_flag and $causeExistsFlag){
+
+        if ($causeExists) {
+            $data = array(
+                'cisID' => $CISID,
+                'start' => $start,
+                'finish' => $end,
+                'causeID' => $causeID,
+                'comment' => $comment,
+                'teamChallenge' => False
+            );
 
             $this->db->insert('time', $data);
-
-
-            return True;
         }
-
-        return False;
 
     }
 
