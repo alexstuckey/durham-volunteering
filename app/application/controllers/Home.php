a2541e51964b8d4ee0886fedd0215c62fd00df46
williamejelias <wj.elias@virgin.net>
2018-03-25 17:20:44 +0100

    make statistics.php the only thing loaded into the frame

--- app/application/controllers/Home.php
+++ app/application/controllers/Home.php
@@ -147,9 +147,9 @@ class Home extends CI_Controller {
         //$this->load->view('leftside', $data);
 
         /* place central column html form chunks within centre_column_open and center_column_close */
-        $this->load->view('center_column_open', $data);
+        //$this->load->view('center_column_open', $data);
         $this->load->view('statistics', $data);
-        $this->load->view('center_column_close', $data);
+        //$this->load->view('center_column_close', $data);
 
         //$this->load->view('rightside', $data);
         //$this->load->view('content_close', $data);
