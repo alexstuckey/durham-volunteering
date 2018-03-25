7483a320fa165224158e04c2d8d54a7ff211a00c
williamejelias <wj.elias@virgin.net>
2018-03-26 00:33:25 +0100

    Revert "making side by side cards the same height"
    This reverts commit c533083bd09b59f10d1107c132f1d9051c0de971.

--- app/application/views/statistics.php
+++ app/application/views/statistics.php
@@ -154,40 +154,36 @@
                 <!-- Numbers Row -->
                 <div class="row">
 
-                    <div class="container">
+                    <!-- Column 1 -->
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Total Hours</h4>
+                            </div>
+                            <div class="card-block">
+                                <div id="progressBar3">
 
-                        <!-- Column 1 -->
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4>Total Hours</h4>
-                                </div>
-                                <div class="card-block">
-                                    <div id="progressBar3">
-
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- End of Column 1 -->
-
-
-                        <!-- Column 2 -->
-                        <div class="col-sm-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4>Favourite cause</h4>
-                                </div>
-                                <div class="card-block">
-                                    <div class="statHeading">
-                                        <h1>RNLI</h1>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End of Column 2 -->
-
                     </div>
+                    <!-- End of Column 1 -->
+
+
+                    <!-- Column 2 -->
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Favourite cause</h4>
+                            </div>
+                            <div class="card-block">
+                                <div class="statHeading">
+                                    <h1>RNLI</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End of Column 2 -->
 
                 </div>
                 <!-- End of Numbers Row -->
