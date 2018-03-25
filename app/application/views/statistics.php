890086530fa8c831462c1dd2a4f1a1f8f8699df4
williamejelias <wj.elias@virgin.net>
2018-03-25 18:18:10 +0100

    remove footers from stats cards and pad department card to fill page better

--- app/application/views/statistics.php
+++ app/application/views/statistics.php
@@ -15,9 +15,7 @@
                     </div>
                     <div class="card-block">
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ac diam ultricies tincidunt. Maecenas venenatis finibus libero sed dapibus. Fusce sit amet nisl a risus commodo placerat. Nam venenatis suscipit orci nec porttitor. Nullam et dui et lacus semper volutpat. Fusce eleifend ornare est vel hendrerit. Etiam aliquet purus</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Footer Information</small>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ac diam ultricies tincidunt. Maecenas venenatis finibus libero sed dapibus. Fusce sit amet nisl a risus commodo placerat. Nam venenatis suscipit orci nec porttitor. Nullam et dui et lacus semper volutpat. Fusce eleifend ornare est vel hendrerit. Etiam aliquet purus</p>
                     </div>
                 </div>
             </div>
@@ -38,9 +36,6 @@
                     <div class="card-block">
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ac diam ultricies tincidunt. Maecenas venenatis finibus libero sed dapibus. Fusce sit amet nisl a risus commodo placerat. Nam venenatis suscipit orci nec porttitor. Nullam et dui et lacus semper volutpat. Fusce eleifend ornare est vel hendrerit. Etiam aliquet purus</p>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Footer Information</small>
-                    </div>
                 </div>
             </div>
             <!-- End of Personal Stats div -->
@@ -53,9 +48,6 @@
                     </div>
                     <div class="card-block">
                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat ac diam ultricies tincidunt. Maecenas venenatis finibus libero sed dapibus. Fusce sit amet nisl a risus commodo placerat. Nam venenatis suscipit orci nec porttitor. Nullam et dui et lacus semper volutpat. Fusce eleifend ornare est vel hendrerit. Etiam aliquet purus</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Footer Information</small>
                     </div>
                 </div>
             </div>
