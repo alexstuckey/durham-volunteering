5c6cd1fbab7f47532324d91133f5f3ba480c5e63
Alex Stuckey <alex@stuckey.org.uk>
2018-03-25 16:25:18 +0100

    Using different method to generate urls

--- app/application/views/volunteering.php
+++ app/application/views/volunteering.php
@@ -10,7 +10,7 @@
     <!-- Link back to My_Volunteering -->
     <div class="card">
         <div class="card-block">
-            <a class="nav-link" href="<?php echo base_url('index.php/my_volunteering'); ?>">Back to Your Volunteering</a>
+            <a class="nav-link" href="<?php echo site_url('/my_volunteering'); ?>">Back to Your Volunteering</a>
         </div>
     </div>
 
