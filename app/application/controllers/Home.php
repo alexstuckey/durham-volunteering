26928485008e7e4beb61f64465797beb85c197f8
AninaAntonie <anina.koehler@live.de>
2018-03-26 10:02:14 +0200

    University style page titles

--- app/application/controllers/Home.php
+++ app/application/controllers/Home.php
@@ -7,6 +7,7 @@ class Home extends CI_Controller {
 	{
         $data['cis_username'] = 'xxxx99';
         $data['active'] = 'home';
+        $data['page_title'] = 'Home - Staff Volunteering Programme';
         $this->load->view('header', $data);
 
         /* place content body chunks within content_open and content_close */
@@ -32,7 +33,7 @@ class Home extends CI_Controller {
 
         $data['cis_username'] = 'xxxx99';
         $data['active'] = 'volunteering';
-        $data['page_title'] = 'My Volunteering';
+        $data['page_title'] = 'My Volunteering - Staff Volunteering Programme';
 
         // populate times array with data accessed from database with time model for logged in user -- $_SERVER['REMOTE_USER']
         $data['times'] = $this->Time_model->getTimeForCIS($_SERVER['REMOTE_USER']);
@@ -62,7 +63,7 @@ class Home extends CI_Controller {
     {
         $data['cis_username'] = 'xxxx99';
         $data['active'] = 'volunteering';
-        $data['page_title'] = 'Activities';
+        $data['page_title'] = 'Activities - Staff Volunteering Programme';
         $this->load->view('header', $data);
 
         $this->load->model('Cause_model');
@@ -92,7 +93,7 @@ class Home extends CI_Controller {
     {
         $data['cis_username'] = 'xxxx99';
         $data['active'] = 'manager';
-        $data['page_title'] = 'Activities';
+        $data['page_title'] = 'Respond to Applications - Staff Volunteering Programme';
         $this->load->view('header', $data);
 
         /* place content body chunks within content_open and content_close */
@@ -114,7 +115,7 @@ class Home extends CI_Controller {
     {
         $data['cis_username'] = 'xxxx99';
         $data['active'] = 'manager';
-        $data['page_title'] = 'Causes';
+        $data['page_title'] = 'Causes - Staff Volunteering Programme';
         $this->load->view('header', $data);
 
         $this->load->model('Cause_model');
@@ -139,7 +140,7 @@ class Home extends CI_Controller {
     {
         $data['cis_username'] = 'xxxx99';
         $data['active'] = 'other';
-        $data['page_title'] = 'Statistics';
+        $data['page_title'] = 'Statistics - Staff Volunteering Programme';
         $this->load->view('header', $data);
 
         /* place content body chunks within content_open and content_close */
