abce7751c4e224d7bbe2cc6c7d92dfcd388f81da
williamejelias <wj.elias@virgin.net>
2018-03-26 22:38:54 +0100

    remove padding and bottom margin on tables in my_volunteering

--- app/application/views/my_volunteering.php
+++ app/application/views/my_volunteering.php
@@ -23,34 +23,36 @@
             <div class="card-header">
                 <h5>Pending Activities</h5>
             </div>
-            <div class="card-block activityTable">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Cause</th>
-                        <th scope="col">Start Time</th>
-                        <th scope="col">End Time</th>
-                        <th scope="col">Comment</th>
-                        <th scope="col">Team Challenge</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <!-- previous activities using if statement if start time is in past -->
-                    <?php foreach ($times as $entries): ?>
-                        <?php if ($entries['status'] == 'pending'): ?>
-                            <tr class="table-warning">
-                                <th scope="row"><?php echo $entries['timeID']; ?></th>
-                                <td><?php echo $entries['causeID']; ?></td>
-                                <td><?php echo $entries['start']; ?></td>
-                                <td><?php echo $entries['finish']; ?></td>
-                                <td><?php echo $entries['comment']; ?></td>
-                                <td><?php echo $entries['teamChallenge']; ?></td>
-                            </tr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
+            <div class="activityTable">
+                <div class="card-block">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Cause</th>
+                            <th scope="col">Start Time</th>
+                            <th scope="col">End Time</th>
+                            <th scope="col">Comment</th>
+                            <th scope="col">Team Challenge</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!-- previous activities using if statement if start time is in past -->
+                        <?php foreach ($times as $entries): ?>
+                            <?php if ($entries['status'] == 'pending'): ?>
+                                <tr class="table-warning">
+                                    <th scope="row"><?php echo $entries['timeID']; ?></th>
+                                    <td><?php echo $entries['causeID']; ?></td>
+                                    <td><?php echo $entries['start']; ?></td>
+                                    <td><?php echo $entries['finish']; ?></td>
+                                    <td><?php echo $entries['comment']; ?></td>
+                                    <td><?php echo $entries['teamChallenge']; ?></td>
+                                </tr>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -62,35 +64,37 @@
             <div class="card-header">
                 <h5>Upcoming Activities</h5>
             </div>
-            <div class="card-block activityTable">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Cause</th>
-                        <th scope="col">Start Time</th>
-                        <th scope="col">End Time</th>
-                        <th scope="col">Comment</th>
-                        <th scope="col">Team Challenge</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <!-- upcoming activities using if statement if start time is in future -->
-                        <!-- replace cause id with organisation relating to that cause id -->
-                        <?php foreach ($times as $entries): ?>
-                            <?php if ($entries['status'] == 'confirmed'): ?>
-                                <tr>
-                                    <th scope="row"><?php echo $entries['timeID']; ?></th>
-                                    <td><?php echo $entries['causeID']; ?></td>
-                                    <td><?php echo $entries['start']; ?></td>
-                                    <td><?php echo $entries['finish']; ?></td>
-                                    <td><?php echo $entries['comment']; ?></td>
-                                    <td><?php echo $entries['teamChallenge']; ?></td>
-                                </tr>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+            <div class="activityTable">
+                <div class="card-block">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Cause</th>
+                            <th scope="col">Start Time</th>
+                            <th scope="col">End Time</th>
+                            <th scope="col">Comment</th>
+                            <th scope="col">Team Challenge</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <!-- upcoming activities using if statement if start time is in future -->
+                            <!-- replace cause id with organisation relating to that cause id -->
+                            <?php foreach ($times as $entries): ?>
+                                <?php if ($entries['status'] == 'confirmed'): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $entries['timeID']; ?></th>
+                                        <td><?php echo $entries['causeID']; ?></td>
+                                        <td><?php echo $entries['start']; ?></td>
+                                        <td><?php echo $entries['finish']; ?></td>
+                                        <td><?php echo $entries['comment']; ?></td>
+                                        <td><?php echo $entries['teamChallenge']; ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -102,34 +106,36 @@
             <div class="card-header">
                 <h5>Previous Activities</h5>
             </div>
-            <div class="card-block activityTable">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Cause</th>
-                        <th scope="col">Start Time</th>
-                        <th scope="col">End Time</th>
-                        <th scope="col">Comment</th>
-                        <th scope="col">Team Challenge</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <!-- previous activities using if statement if start time is in past -->
-                        <?php foreach ($times as $entries): ?>
-                            <?php if ($entries['status'] == 'confirmed'): ?>
-                                <tr>
-                                    <th scope="row"><?php echo $entries['timeID']; ?></th>
-                                    <td><?php echo $entries['causeID']; ?></td>
-                                    <td><?php echo $entries['start']; ?></td>
-                                    <td><?php echo $entries['finish']; ?></td>
-                                    <td><?php echo $entries['comment']; ?></td>
-                                    <td><?php echo $entries['teamChallenge']; ?></td>
-                                </tr>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+            <div class="activityTable">
+                <div class="card-block">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Cause</th>
+                            <th scope="col">Start Time</th>
+                            <th scope="col">End Time</th>
+                            <th scope="col">Comment</th>
+                            <th scope="col">Team Challenge</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <!-- previous activities using if statement if start time is in past -->
+                            <?php foreach ($times as $entries): ?>
+                                <?php if ($entries['status'] == 'confirmed'): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $entries['timeID']; ?></th>
+                                        <td><?php echo $entries['causeID']; ?></td>
+                                        <td><?php echo $entries['start']; ?></td>
+                                        <td><?php echo $entries['finish']; ?></td>
+                                        <td><?php echo $entries['comment']; ?></td>
+                                        <td><?php echo $entries['teamChallenge']; ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -141,34 +147,36 @@
             <div class="card-header">
                 <h5>Denied Activities</h5>
             </div>
-            <div class="card-block activityTable">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Cause</th>
-                        <th scope="col">Start Time</th>
-                        <th scope="col">End Time</th>
-                        <th scope="col">Comment</th>
-                        <th scope="col">Team Challenge</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <!-- denied activities using if statement - status is 'denied' -->
-                    <?php foreach ($times as $entries): ?>
-                        <?php if ($entries['status'] == 'denied'): ?>
-                            <tr class="table-danger">
-                                <th scope="row"><?php echo $entries['timeID']; ?></th>
-                                <td><?php echo $entries['causeID']; ?></td>
-                                <td><?php echo $entries['start']; ?></td>
-                                <td><?php echo $entries['finish']; ?></td>
-                                <td><?php echo $entries['comment']; ?></td>
-                                <td><?php echo $entries['teamChallenge']; ?></td>
-                            </tr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
+            <div class="activityTable">
+                <div class="card-block">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Cause</th>
+                            <th scope="col">Start Time</th>
+                            <th scope="col">End Time</th>
+                            <th scope="col">Comment</th>
+                            <th scope="col">Team Challenge</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!-- denied activities using if statement - status is 'denied' -->
+                        <?php foreach ($times as $entries): ?>
+                            <?php if ($entries['status'] == 'denied'): ?>
+                                <tr class="table-danger">
+                                    <th scope="row"><?php echo $entries['timeID']; ?></th>
+                                    <td><?php echo $entries['causeID']; ?></td>
+                                    <td><?php echo $entries['start']; ?></td>
+                                    <td><?php echo $entries['finish']; ?></td>
+                                    <td><?php echo $entries['comment']; ?></td>
+                                    <td><?php echo $entries['teamChallenge']; ?></td>
+                                </tr>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
