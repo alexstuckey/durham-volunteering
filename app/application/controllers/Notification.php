d79b9a7cff4c2bf2744e32b74f160dd4b49855cc
williamejelias <wj.elias@virgin.net>
2018-03-29 18:58:52 +0100

    removed void create notification function in controller as a notification will only ever be auto generated upon submit of other forms

--- app/application/controllers/Notification.php
+++ app/application/controllers/Notification.php
@@ -4,22 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Notification extends CI_Controller {
 
 
-    public function createNotificationSubmit()
-    {
-        // call model - /createNotification
-        $this->load->model('Notification_model');
-
-        $this->Notification_model->createNotification(
-            $_SERVER['REMOTE_USER'],
-            'title',
-            'blurb',
-            'time'
-        );
-
-        //$this->Audit_model->insertLog('CREATE', 'Notification created.');
-
-    }
-
     public function deleteNotificationSubmit()
     {
         // call model - /deleteNotification
