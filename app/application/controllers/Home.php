cfec415e50c072418a7266be02507a193d485f82
Alex Stuckey <alex@stuckey.org.uk>
2018-03-30 18:04:25 +0100

    Referencing the original element, not a shadow copy

--- app/application/controllers/Home.php
+++ app/application/controllers/Home.php
@@ -110,8 +110,8 @@ class Home extends CI_Controller {
 
         $data['managees'] = $this->User_model->getManagees($_SERVER['REMOTE_USER']);
         $data['manageesNominated'] = $this->User_model->getManageesNominated($_SERVER['REMOTE_USER']);
-        foreach ($data['managees'] as $managee) {
-            $managee['times'] = $this->Time_model->getTimeForCIS($managee['cisID']);
+        foreach ($data['managees'] as $key => $managee) {
+            $data['managees'][$key]['times'] = $this->Time_model->getTimeForCIS($managee['cisID']);
         }
         
         $data['causes'] = $this->Cause_model->getAllCauses();
