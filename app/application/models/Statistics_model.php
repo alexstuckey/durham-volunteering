7fb4a91da9227fbd60a7d565b1cab37a001c7905
original <jonathan.a.digweed@durham.ac.uk>
2018-03-30 18:34:00 +0100

    Better return values.

--- app/application/models/Statistics_model.php
+++ app/application/models/Statistics_model.php
@@ -12,20 +12,18 @@ class Statistics_model extends CI_Model {
     public function sumTimeByCause()
     {
 
+        $query = $this->db->query("SELECT organisation, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum  FROM times JOIN causes WHERE times.causeID=causes.causeID AND status='confirmed' GROUP BY causes.organisation ORDER BY timeSum DESC ");
 
-    $query = $this->db->query("SELECT organisation, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum  FROM times JOIN causes WHERE times.causeID=causes.causeID AND status='completed' GROUP BY causes.organisation");
-
-    return $query->result_array();
+        return $query->result_array();
 
     }
 
     public function volunteeringTimebyDepartment()
     {
 
-    $query = $this->db->query("SELECT departmentsName, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum FROM times join users ON times.cisID=users.cisID join departments ON users.departmentID=departments.ID WHERE status='completed' group by departmentsName");
+        $query = $this->db->query("SELECT departmentsName, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum FROM times join users ON times.cisID=users.cisID join departments ON users.departmentID=departments.ID WHERE status='confirmed' group by departmentsName ORDER BY timeSum DESC");
 
-
-    return $query->result_array();
+        return $query->result_array();
 
     }
 
@@ -36,7 +34,7 @@ class Statistics_model extends CI_Model {
 
         $data= $query->result_array();
 
-        return $data;
+        return (string) $data[0]['timeSum'];
 
     }
 
@@ -45,17 +43,15 @@ class Statistics_model extends CI_Model {
         $query = $this->db->query("SELECT SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum FROM times WHERE status='confirmed' ");
         $data= $query->row_array();
 
-
-        return $data;
-
+        return (string) $data['timeSum'];
     }
 
     public function totalVolunteers()
     {
         $query = $this->db->query("SELECT count(*) FROM users");
         $data= $query->row_array();
-        
-        return $data;
+
+        return (string) $data['count(*)'];
 
     }
 
@@ -63,12 +59,9 @@ class Statistics_model extends CI_Model {
     {
         $query = $this->db->query("SELECT cisID, organisation, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum  FROM times JOIN causes WHERE times.causeID=causes.causeID AND status='confirmed' AND ". $cisID . "=times.cisID  GROUP BY causes.organisation order by timeSum desc limit 1");
 
-
         $data= $query->row_array();
 
-
-
-        return $data;
+        return (string) $data['organisation'];
     }
 
     public function positionWithinDepartment($cisID)
@@ -78,11 +71,27 @@ class Statistics_model extends CI_Model {
 
         $department=$this->User_model->getDepartment($cisID);
 
-        $query = $this->db->query("SELECT users.cisID,departmentsName, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum FROM times join users ON times.cisID=users.cisID join departments ON users.departmentID=departments.ID WHERE times.status='confirmed' AND departmentsName=\"" .$department."\" group by cisID,departmentsName ORDER BY timeSum desc");
+        $query = $this->db->query("SELECT users.cisID,departmentsName, SEC_TO_TIME( SUM( TIME_TO_SEC( TIMEDIFF(finish,start) ) ) ) AS timeSum FROM times join users ON times.cisID=users.cisID join departments ON users.departmentID=departments.ID WHERE times.status='confirmed' AND departmentsName=\"" .$department."\" group by cisID,departmentsName ORDER BY timeSum DESC");
 
-        $data= $query->row_array();
+        $data= $query->result_array();
+
+        $total = sizeof($data)+1;
 
 
-        return $data;
+        $i = 1;
+
+        foreach ($data as $arr)
+        {
+            if ($arr['cisID']==$cisID){
+                print("You are ". $i . " out of " . $total);
+            }
+
+            $i+=1;
+
+        }
+
+        $return = array($i,$total);
+
+        return $return;
     }
 }
