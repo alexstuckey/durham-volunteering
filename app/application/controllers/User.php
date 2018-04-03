ed61e0eb4141210fddea6d13a7a631373275162b
Alex Stuckey <alex@stuckey.org.uk>
2018-04-03 19:21:54 +0100

    Cleaning up controllers

--- app/application/controllers/User.php
+++ app/application/controllers/User.php
@@ -3,16 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
-	public function index()
-	{
-		echo 'Invalid URL';
-	}
+    public function index()
+    {
+        echo 'Invalid URL';
+    }
 
-	public function get($userCISID)
-	{
+    public function get($userCISID)
+    {
         $this->load->model('User_model');
         $data = $this->User_model->getUserObjectByCIS($userCISID);
 
-	}
+    }
 
 }
