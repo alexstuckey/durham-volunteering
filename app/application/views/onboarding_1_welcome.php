277131b908782e999664d86dbe4f007f4dd9a2fe
AninaAntonie <anina.koehler@live.de>
2018-04-03 02:41:24 +0200

    welcome page

--- app/application/views/onboarding_1_welcome.php
+++ app/application/views/onboarding_1_welcome.php
@@ -1,7 +1,7 @@
 
 
 
-            <div class="text-center" id="page-heading" style="margin-left:20px; margin-right: auto;">
+            <div class="text-center" id="page-heading">
                 <h1 class="onboardh1">Welcome!</h1>
                 <h6>You are registering as username <?php echo $user['username'] ?>.</h6>
                 <p><br />Thank you for signing up to Durham University's Volunteering Project.<br />With this account you can organize and track your volunteering activities.</p>
@@ -9,13 +9,5 @@
                     <button class="btn btn-primary">Continue</button>
                 </a>
             </div>
-            <div class="container" style="min-height:100px;">
-                <div class="row">
-                    <div class="col-lg-4 mb-4">
-                    </div>
-                    <div class="col-lg-4 mb-4">
-                    </div>
-                    <div class="col-lg-4 mb-4">
-                    </div>
-                </div>
+            <div class="container" style="min-height:200px;">
             </div>
